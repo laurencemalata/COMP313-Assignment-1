f6cd17473115c69f3022e0dc3e336693114cd812
laurencemalata <laurence.malata@gmail.com>
2021-07-23 01:50:43 +1200

    Changes to assets

---
+++ Source/Assignment_1/Assignment_1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Assignment_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Assignment_1, "Assignment_1" );
